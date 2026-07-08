5ff56f19d17a56990ed34196a6b0a7c92d14e565
chimw <chimw@lamcs.coop>
2026-07-08 23:40:01 +0200

    add webpack polyfill fallbacks for edge runtime

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,18 @@ const nextConfig = {
       bodySizeLimit: "2mb",
     },
   },
+  webpack: (config, { nextRuntime }) => {
+    if (nextRuntime === "edge") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: false,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
